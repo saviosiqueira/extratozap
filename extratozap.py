01321aeb918c8d59327f9fd8e729015e1b5604c4
Savio Siqueira <savioosiqueira@outlook.com>
2021-01-19 18:08:31 -0300

    Extraindo nome e numero dos grupos

--- extratozap.py
+++ extratozap.py
@@ -10,7 +10,15 @@ class Grupo:
         self.usuarios = '' # Usuarios.Extrair_usuarios()
 
     def Extrair_nome_numero(self):
-        ...
+        #Padrão do regex:
+        padrao_grupo = r"Conversa do WhatsApp com (\[(\d{1,})\].*)"
+        resultado = re.search(padrao_grupo, self.arquivo, re.MULTILINE)
+        
+        nome = resultado.group(1)
+        numero = resultado.group(2)
+
+        return nome, numero
+
 
     def Exportar_Contatos(self):
         ...
@@ -25,6 +33,15 @@ class Grupo:
 if __name__ == '__main__':
     arquivos_grupos = Grupo.Importar_Grupos()
 
-    arquivos_grupos = arquivos_grupos[0]
-    pprint(arquivos_grupos)
+    arquivos_grupo = arquivos_grupos[0]
+    #pprint(arquivos_grupo)
+
+    #grupo = Grupo(arquivos_grupo)
+    #print(f'Grupo: {grupo.nome} \n-Número: {grupo.numero}')
+
+    for grupo in arquivos_grupos:
+        grupo = Grupo(grupo)
+        print(f'Grupo: {grupo.nome} \n-Número: {grupo.numero}')
+    
+
 
